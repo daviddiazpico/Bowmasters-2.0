afa7e69d89b7214d202be658e3c85ecac75e21e5
daviddiazpico <daviddiazpico@gmail.com>
2024-05-06 17:19:54 +0200

    Disparo flecha correcto y animaciones personaje correctas

--- scripts/Flecha.cs
+++ scripts/Flecha.cs
@@ -3,9 +3,12 @@ using System;
 
 public partial class Flecha : RigidBody2D
 {
+	AnimatedSprite2D anim_Flecha;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		this.Position = new Vector2(10, -10);
+		anim_Flecha = GetNode<AnimatedSprite2D>("SpriteFlecha");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,8 +16,10 @@ public partial class Flecha : RigidBody2D
 	{
 	}
 
-    public void Mover()
-    {
-        Position += new Vector2(50, 0);
-    }
+	public void Disparar()
+	{
+		//anim_Flecha.Play();
+		Vector2 velocidadFlecha = new Vector2(1000, -400);
+		this.LinearVelocity = velocidadFlecha;
+	}
 }
--- scripts/Jugador1.cs
+++ scripts/Jugador1.cs
@@ -1,19 +1,25 @@
 using Godot;
 using System;
+using System.Threading;
 
 public partial class Jugador1 : CharacterBody2D
 {
-	AnimatedSprite2D animacionDisparar;
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	AnimatedSprite2D anim_CargarDisparo;
+	AnimatedSprite2D anim_AguantarDisparo;
+	AnimatedSprite2D anim_Disparar;
 	PackedScene flecha;
+	bool disparoCargado;
 
-    public override void _Ready()
-    {
-        Position = new Vector2(157, 200);
-		animacionDisparar = GetNode<AnimatedSprite2D>("SpriteJugador1");
+	public override void _Ready()
+	{
+		Position = new Vector2(157, 200);
+		anim_CargarDisparo = GetNode<AnimatedSprite2D>("Sprite1Jugador1");
+		anim_AguantarDisparo = GetNode<AnimatedSprite2D>("Sprite2Jugador1");
+		anim_Disparar = GetNode<AnimatedSprite2D>("Sprite3Jugador1");
 		flecha = (PackedScene)ResourceLoader.Load("res://escenas/Flecha.tscn");
-    }
-    // Get the gravity from the project settings to be synced with RigidBody nodes.
-    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -26,33 +32,87 @@ public partial class Jugador1 : CharacterBody2D
 		Velocity = velocity;
 		MoveAndSlide();
 
-		if (Input.IsActionJustPressed("espacio"))
+		if (Input.IsActionPressed("espacio"))
 		{
-			Atacar();
+			anim_CargarDisparo.Play();
 		}
-		else
+		else if (Input.IsActionJustReleased("espacio"))
 		{
-			DejarAtacar();
+			if (disparoCargado)
+			{
+				Atacar();
+			}
+			else
+			{
+				DesactivarTodasLasAnimaciones();
+			}
+			
 		}
 	}
 
+	private void DesactivarTodasLasAnimaciones()
+	{
+		anim_CargarDisparo.Stop();
+		anim_AguantarDisparo.Stop();
+		anim_AguantarDisparo.Visible = false;
+		anim_Disparar.Stop();
+		anim_Disparar.Visible = false;
+	}
+
+	private void OnAnimationSprite1Looped()
+	{
+		anim_CargarDisparo.Stop();
+		anim_CargarDisparo.Visible = false;
+
+		anim_AguantarDisparo.Visible = true;
+		anim_AguantarDisparo.Play();
+		disparoCargado = true;
+	}
+
+	private void OnAnimationSprite3Looped()
+	{
+		anim_Disparar.Stop();
+		anim_Disparar.Visible = false;
+
+		anim_CargarDisparo.Stop();
+		anim_CargarDisparo.Visible = true;
+	}
+
+	private void ActivarAnimacionDisparar()
+	{
+		anim_AguantarDisparo.Stop();
+		anim_AguantarDisparo.Visible = false;
+		
+		anim_Disparar.Visible = true;
+		anim_Disparar.Play();
+	}
+
 	private void Atacar()
+	{
+		ActivarAnimacionDisparar();
+		Flecha nuevaFlecha = CrearFlecha();
+
+		if (nuevaFlecha != null)
+		{
+			nuevaFlecha.Disparar();
+			disparoCargado = false;
+		}
+		else
+		{
+			GD.Print("Flecha instanciada == null");
+		}
+	}
+
+	private Flecha CrearFlecha()
 	{
 		if (flecha != null)
 		{
 			Flecha nuevaFlecha = flecha.Instantiate<Flecha>();
 			this.AddChild(nuevaFlecha);
-			nuevaFlecha.Position = new Vector2(10, 10);
-			//nuevaFlecha.Mover();
-			animacionDisparar.Play();
-		}
-		
-		
-	}
 
-	private void DejarAtacar()
-	{
-		flecha.IsQueuedForDeletion();
-		animacionDisparar.Stop();
+			return nuevaFlecha;
+		}
+
+		return null;
 	}
 }
